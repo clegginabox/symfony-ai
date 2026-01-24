68626c41e165217b32a41a47dde27bec845cb7af
Paul Clegg <pclegg@gmail.com>
2026-01-24 14:08:48 +0000

    [Platform] Load Balanced Platform proof of concept

---
+++ src/platform/src/Bridge/LoadBalanced/Strategy/RandomStrategy.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Platform\Bridge\LoadBalanced\Strategy;
+
+/**
+ * Randomises the platform selection order.
+ *
+ * @author Paul Clegg <hello@clegginabox.co.uk>
+ */
+final class RandomStrategy implements PlatformSelectionStrategy
+{
+    public function order(array $platforms): iterable
+    {
+        shuffle($platforms);
+
+        return $platforms;
+    }
+}
